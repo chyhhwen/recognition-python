7292d0a8701a9771da45523c7ac779e10481169e
chyhhwen <chyhhwen@gmail.com>
2022-09-26 21:53:42 +0800

    test

--- project2.py
+++ project2.py
@@ -1,30 +1,41 @@
 import cv2
+
 cap = cv2.VideoCapture(0)
+
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
 while True:
     ret, frame = cap.read()
+
     if not ret:
         print("Cannot receive frame")
         break
     # 套用自適應二值化黑白影像
     img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY);
+
     img_gray = cv2.medianBlur(img_gray, 5)
+
     output = cv2.adaptiveThreshold(img_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+
     faceCascade = cv2.CascadeClassifier('face_detect.xml')
     faceRect = faceCascade.detectMultiScale(img_gray, 2, 3)
+
     faceCascade1 = cv2.CascadeClassifier('car.xml')
     faceRect1 = faceCascade1.detectMultiScale(img_gray, 1.1, 2)
+
     for (x, y, w, h) in faceRect:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
         cv2.putText(frame, 'face', (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
     for (x, y, w, h) in faceRect1:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         cv2.putText(frame, 'car', (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-    #cv2.imshow('oxxostudio', output)
+
     cv2.imshow('video', frame)
     if cv2.waitKey(1) == ord('q'):
         break       # 按下 q 鍵停止
+
 cap.release()
+
 cv2.destroyAllWindows()
